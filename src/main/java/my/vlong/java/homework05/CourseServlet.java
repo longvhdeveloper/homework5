5205c0b3a1db8e4fd5936af0d8ce73d394806856
Vo Hoang Long <vohoanglong07@gmail.com>
2019-01-22 00:03:31 +0700

    Inititalize homework5

---
+++ src/main/java/my/vlong/java/homework05/CourseServlet.java
@@ -0,0 +1,20 @@
+package my.vlong.java.homework05;
+
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(value="/courses", name="courseServlet")
+public class CourseServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        RequestDispatcher view = req.getRequestDispatcher("course/list.jsp");
+        view.forward(req, resp);
+    }
+
+}
---
+++ src/main/webapp/course/list.jsp
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>ABC Schools</title>
+</head>
+<body>
+<h1>List course</h1>
+</body>
